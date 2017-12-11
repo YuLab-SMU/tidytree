40dd23aafaa4962092195ca57e3562c705aef4d2
guangchuang yu <guangchuangyu@gamil.com>
2017-12-11 20:54:32 +0800

    re-export dplyr verbs

--- R/reexports.R
+++ R/reexports.R
@@ -10,6 +10,34 @@ magrittr::`%<>%`
 ##' @export
 tibble::as_data_frame
 
+##' @importFrom dplyr filter
+##' @export
+dplyr::filter
+
+##' @importFrom dplyr arrange
+##' @export
+dplyr::arrange
+
+##' @importFrom dplyr select
+##' @export
+dplyr::select
+
+##' @importFrom dplyr rename
+##' @export
+dplyr::rename
+
+##' @importFrom dplyr mutate
+##' @export
+dplyr::mutate
+
+##' @importFrom dplyr transmute
+##' @export
+dplyr::transmute
+
+##' @importFrom dplyr summarise
+##' @export
+dplyr::summarise
+
 ##' @importFrom treeio groupOTU
 ##' @export
 treeio::groupOTU
