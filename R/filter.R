a56153ee0453088f4aa88b48cce4d5b8e7942314
xiangpin <xshuangbin@163.com>
2021-08-20 20:40:53 +0800

    add filter, select, mutate for treedata object

--- R/filter.R
+++ R/filter.R
@@ -7,3 +7,15 @@ filter.ggtree <- function(.data, ..., .preserve = FALSE) {
     dplyr::filter(.data$data, !!!dots, .preserve = .preserve)
 }
 
+##' @method filter treedata
+##' @export
+filter.treedata <- function(.data, ..., .preserve=FALSE, keep.td=TRUE){
+    dots <- rlang::quos(...)
+    dat <- .extract_annotda.treedata(.data)
+    da <- dplyr::filter(dat, !!!dots, .preserve = .preserve)
+    if (keep.td){
+        .data <- .update.treedata(td=.data, da=da, dat=dat, type="extra")
+        return(.data)
+    }
+    return(da)
+}
--- R/get-fields.R
+++ R/get-fields.R
@@ -1,7 +1,5 @@
-##' get.fields method
-##'
-##'
 ##' @rdname get.fields-methods
+##' @aliases get.fields,treedata
 ##' @exportMethod get.fields
 setMethod("get.fields", signature(object = "treedata"),
           function(object) {
@@ -10,20 +8,30 @@ setMethod("get.fields", signature(object = "treedata"),
 
 
 get.fields.treedata <- function(object) {
+    fields1 <- get.fields.data(object)
+    fields2 <- get.fields.extraInfo(object)
+    return(c(fields1, fields2))
+}
+
+get.fields.data <- function(object){
     if (nrow(object@data) > 0) {
         fields <- colnames(object@data)
         fields <- fields[fields != "node"]
     } else {
         fields <- ""
     }
+    return(fields)
+}
 
+get.fields.extraInfo <- function(object){
     extraInfo <- object@extraInfo
     if (nrow(extraInfo) > 0) {
         cn <- colnames(extraInfo)
         i <- match(c("x", "y", "isTip", "node", "parent", "label", "branch", "branch.length"), cn)
         i <- i[!is.na(i)]
-        fields %<>% c(cn[-i])
+        fields <- cn[-i]
+        return(fields)
+    }else{
+        return(character(0))
     }
-    return(fields)
 }
-
