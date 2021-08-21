4edab3d002c9edc61ee0212041c25a3255e3462c
xiangpin <xshuangbin@163.com>
2021-08-21 19:39:57 +0800

    add pull for treedata

---
+++ R/pull.R
@@ -0,0 +1,12 @@
+##' @method pull treedata
+##' @export
+pull.treedata <- function(.data, var = -1, name = NULL, ...){
+    var <- rlang::enquo(var)
+    name <- rlang::enquo(name)
+    dat <- .extract_annotda.treedata(.data)
+    dplyr::pull(dat, var = !!var, name = !!name, ...)
+}
+
+##' @method pull phylo
+##' @export
+pull.phylo <- pull.treedata
